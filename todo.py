666c260a35c08e59af4dd0ed8df6ad8af65f45f0
bmanojna <bmanojna2007@gmail.com>
2026-03-18 00:04:16 +0530

    Add files via upload

---
+++ todo.py
@@ -0,0 +1,17 @@
+# todolist by using conditions
+# 1.writing out the task
+# 2.viewing of our task
+# 3.editing of our task
+# 4.deleting of our task
+def char(arg):
+
+task = (char(input("enter your task: ")))
+task = ("daily hablits")
+if task == 1:
+   print("enter your task: ")
+elif task == 2:
+   print(view the task,"print task == 1") # type: ignore
+elif task == 3:
+   print("edit your task")
+else :
+   print("deleted task")
