3bbd963a4e2ccd5f53a52d00b2cd82846045957c
bmanojna <bmanojna2007@gmail.com>
2026-03-18 17:52:05 +0530

    Add files via upload

--- todo.py
+++ todo.py
@@ -1,17 +1,39 @@
 # todolist by using conditions
-# 1.writing out the task
-# 2.viewing of our task
-# 3.editing of our task
-# 4.deleting of our task
-def char(arg):
 
-task = (char(input("enter your task: ")))
-task = ("daily hablits")
-if task == 1:
-   print("enter your task: ")
-elif task == 2:
-   print(view the task,"print task == 1") # type: ignore
-elif task == 3:
-   print("edit your task")
-else :
-   print("deleted task")
+tasks = []
+while True:
+   print("1.enter your task")
+   print("2.view your task")
+   print("3.delete your task")
+   print("4.exit")
+
+   choice = (int(input("enter choice: ")))
+   if choice == 1:
+       task = (input("enter task"))
+       tasks.append(task)
+       print("task added!")
+   elif choice == 2:
+       if len(tasks) == 0:
+           print("no tasks avaliable")
+       else:
+           print("\nyour tasks")
+           for i in range(len(tasks)):
+               print(f"{i+1}. {tasks[i]}")
+   elif choice == 3:
+       if len(tasks)==0: 
+           print("there is no task to delete")
+       else:
+           for i in range(len(tasks)):
+               print(f"{i+1}.{tasks[i]}")
+           num = int(input("enter your task number to delete: "))
+           if 1 <= num <= len(tasks):
+               tasks.pop(num-1)
+               print("tasks deleted!")
+           else:
+               print("invalid task")
+   elif choice == 4:
+       print("good bye!")
+       break
+   else:
+       print("invalid choice")
+
